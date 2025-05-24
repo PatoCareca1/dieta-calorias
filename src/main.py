3125913f6a934e6dc65f29b53f4fd9f2de4675de
PatoCareca1 <LucasDan16@outlook.com>
2025-05-24 15:24:56 -0300

    fix: cria tabelas no evento de startup da API

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
+# src/main.py
 import uvicorn
 from fastapi import FastAPI
-from src.api.routers import users, calorias, alimentos  # importa os routers
+# Base e engine
+from src.database import Base, engine  
+from src.api.routers import users, calorias, alimentos
 
 app = FastAPI(title="API Dieta-Calorias", version="0.1.0")
 
-# inclui routers
+# routers
 app.include_router(users.router)
 app.include_router(calorias.router)
 app.include_router(alimentos.router)
+
+@app.on_event("startup")
+def on_startup():
+    """
+    Cria (se não existirem) todas as tabelas do nosso ORM
+    sempre que a aplicação iniciar.
+    """
+    Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
